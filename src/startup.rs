fc3f7f2f5bb88cc9a93a2f29548e8ccc4e48fade
michael <michael.dewey131@gmail.com>
2023-06-13 16:18:23 -0700

    Incorporates changes from todays dev time

---
+++ src/startup.rs
@@ -0,0 +1,21 @@
+use actix_web::{
+   App,
+   web,
+   dev::Server,
+   HttpServer,
+};
+use std::net::TcpListener;
+
+use crate::routes::health_check::*;
+use crate::routes::subscriptions::*;
+
+pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
+    let server = HttpServer::new(|| {
+            App::new()
+                .route("/health_check", web::get().to(health_check))
+                .route("/subscriptions", web::post().to(subscribe))
+        })
+        .listen(listener)?
+        .run();
+    Ok(server)
+}
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -7,7 +7,7 @@ fn spawn_app() -> String {
         .expect("Failed to bind to random port");
     // Retrieve the port assigned by the OS
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     // Launch the server as a background task
     // tokio::spawn returns a handle to the spawned future, 
     // but we have no use for it here, hence the non-binding let
